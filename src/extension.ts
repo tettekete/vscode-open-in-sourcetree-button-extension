e60c070b9f5481bc19bd5b049d5c46ead8c65a8d
tettekete <6535634+tettekete@users.noreply.github.com>
2024-12-20 10:35:13 +0900

    feat: add feature to select SourceTree in file selection dialog and save to config
    - Implemented functionality to choose SourceTree via a file selection dialog.
    - Updated the configuration to save the selected SourceTree path.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { openSourcetree } from './open-sourcetree';
+import path from 'node:path';
 
 import {
 	CONFIG_SOURCETREE_PATH,
@@ -24,7 +25,45 @@ export function activate(context: vscode.ExtensionContext)
 
 	// registerCommand
 	const disposable = vscode.commands.registerCommand('tettekete.openSourcetreeButton', openSourcetree);
-	context.subscriptions.push(disposable, statusBarItem ,configChangeListener );
+	const setPathCommand = vscode.commands.registerCommand('tettekete.setSourceTreePath',setSourcetreePathConfig );
+
+	context.subscriptions.push(
+						disposable,
+						statusBarItem,
+						configChangeListener,
+						setPathCommand
+					);
+}
+
+async function setSourcetreePathConfig()
+{
+	const result = await vscode.window.showOpenDialog({
+		canSelectMany: false,
+		canSelectFiles: true,
+		canSelectFolders: false,
+		filters: {
+			Executable: process.platform === 'win32' ? ['exe'] : [],
+		},
+		openLabel: 'select',
+		title: vscode.l10n.t('Select SourceTree Executable')
+	});
+
+	if (result && result[0]) {
+		const selectedPath = result[0].fsPath;
+		if( ! /^sourcetree\.(?:exe)|(?:app)$/i.test( path.basename( selectedPath )) )
+		{
+			vscode.window.showErrorMessage(
+				vscode.l10n.t('The specified path does not point to the Sourcetree application. For security reasons, only SourceTree.exe or SourceTree.app can be specified.\n"{0}"',selectedPath ),
+				{modal: true}
+			);
+		}
+		else
+		{
+			await vscode.workspace.getConfiguration().update(CONFIG_SOURCETREE_PATH, selectedPath, vscode.ConfigurationTarget.Global);
+			vscode.window.showInformationMessage(`SourceTree path set to: ${selectedPath}`);
+		}
+	}
+	
 }
 
 function updateStatusBarIcon()
